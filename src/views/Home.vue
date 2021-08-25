160cbf97df77e9bd678145e3dfdc0b3d68574e1e
Methujan <methujan_02@hotmail.com>
2021-08-25 00:27:04 -0400

    add styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,96 @@
 <template>
   <div class="home">
-  Home
+    <div class="feature-card">
+      <router-link to="/movie/tt5611026">
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BMTcyZjlkYWEtMTg5ZC00NDFlLWE4NjItMzYxNDc4M2ZmYTAwXkEyXkFqcGdeQXVyNzk2ODQxMDg@._V1_SX300.jpg"
+          alt="One Piece"
+          class="featured-img"
+        />
+        <div class="detail">
+          <h3>One Piece</h3>
+          <p>
+            The young pirate Monkey D Luffy sets out on a quest in search of the
+            legendary treasure One Piece.
+          </p>
+        </div>
+      </router-link>
+    </div>
+
+    <form @submit.prevent="" class="search-box">
+      <input type="text" placeholder="What are you looking for?" />
+      <input type="submit" value="Search" />
+    </form>
+
+    <div class="movies-list">MOVIES</div>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+      position: relative;
+      z-index: 0;
+    }
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #fff;
+      }
+    }
+  }
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+
+    input {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      $[type="text"] {
+        width: 100%;
+        color: #fff;
+        background-color: #496583;
+        font-size: 20px;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
+
+        &::placeholder {
+          color: #f3f3f3;
+        }
+      }
+    }
+  }
+}
+</style>
